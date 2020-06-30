d2928195c24e5333181cde8602195c0ff219ed41
LuciusMuchachos <62340520+LuciusMuchachos@users.noreply.github.com>
2020-06-30 18:13:42 +0300

    Second Variation

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public int speed = 5;
+    public int speedRotation = 3;
+    public AnimationClip anima;
+    public AnimationClip anima2;
+    public int jumpSpeed = 50;
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.Rotate(Vector3.down * speedRotation);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Rotate(Vector3.up * speedRotation);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.transform.position += player.transform.up * jumpSpeed * Time.deltaTime;
+        }
+    }
+}
